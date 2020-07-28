ec293e5aacc0a2caa210505d8c9fe2f0bdc7b8f6
arthur hauser <arthur.hauser@gmail.com>
2020-07-27 20:00:59 -0400

    burger

---
+++ master/BurgerSolution/db/seeds.sql
@@ -0,0 +1,5 @@
+-- 3. Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
---
+++ myBurgerhw/db/seeds.sql
@@ -0,0 +1,8 @@
+-- 3. Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+-- this code below was provided by Trinity
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
